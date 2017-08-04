266c549d33eec5eb8cbe1b85214eb22dff0f1496
Fahad Haidari <fahad.haidari@gmail.com>
2017-08-04 14:55:17 -0600

    updated the logic

--- sockets/index.js
+++ sockets/index.js
@@ -1,15 +1,14 @@
 module.exports = function(io) {
-
-
   var users = [];
   var rooms = [];
   var roomNames = [];
 
   io.on('connection', function(socket) {
     socket.on('on message', function(msg) {
-      // var obj = JSON.parse(msg);
       var obj = msg;
       console.log("room ", obj.room, " user", obj.user);
+      // io.to(obj.room).emit('on message', obj.msg);
+      obj.id = socket.id;
       io.to(obj.room).emit('on message', obj.msg);
     });
 
@@ -19,58 +18,34 @@ module.exports = function(io) {
     });
 
     socket.on('room', function(info) {
-      // var obj = JSON.parse(info);
       var obj = info;
-
-      // if(users.indexOf(obj.user) == -1) {
       console.log("Joining room ->", obj.room);
       console.log("Users joined the room ", users);
-      socket.join(obj.room);
+      socket.join(obj.room.name);
       users.push(obj.user);
 
-      var indexOfRoomName = roomNames.indexOf(obj.room);
+      var indexOfRoomName = roomNames.indexOf(obj.room.name);
 
-      var room = {};
+      if (indexOfRoomName == -1) {
+        roomNames.push(obj.room.name);
 
-      if(indexOfRoomName == -1) {
-        roomNames.push(obj.room);
-        room = {
-          name: obj.room,
-          users: []
-        }
-        rooms.push(room);
+        rooms.push(obj.room);
       } else {
-        room = rooms[indexOfRoomName];
+        obj.room = rooms[indexOfRoomName];
       }
-
-
 
       let user = {
         name: obj.user
       }
 
+      obj.room.users.push(user);
 
-
-      room.users.push(user);
-
-
-      //
-      // if (rooms.indexOf(room) == -1) {
-      //   rooms.push(room);
-      // }
-
-      // console.log("Rooms data ", rooms);
 
       for (var i = 0; i < rooms.length; i++) {
         console.log("Room ", rooms[i].name, " Users ", rooms[i].users);
-        // console.log("Room users", rooms[i].users);
       }
 
-
-      io.to(obj.room).emit('room', obj);
-      // } else {
-      // io.to(obj.room).emit('no join', "User with this name does exist...");
-      // }
+      io.to(obj.room.name).emit('room', obj);
 
     });
 
