6cfe88286095a56bf8eae59a83b44887683bdc27
Matthew Gonzer <matthewgonzer@gmail.com>
2017-08-08 11:51:05 -0600

    rooms

--- seeds/02_room.js
+++ seeds/02_room.js
@@ -3,10 +3,12 @@ exports.seed = function(knex, Promise) {
   return knex.raw('TRUNCATE room RESTART IDENTITY CASCADE;')
     .then(function () {
       return knex('room').insert([
-        {name: 'Coders', max_numplayers: 2},
-        {name: 'Developers', max_numplayers: 4},
-        {name: 'Hackers', max_numplayers: 3},
-        {name: 'CJ\'s disciples'}
+        {name: 'Denver Developers', max_numplayers: 2},
+        {name: 'Colorado Coders', max_numplayers: 4},
+        {name: 'Galvanize Alumni', max_numplayers: 3},
+        {name: 'Pittsburgh Programmers', max_numplayers: 2},
+        {name: 'Hoboken Hackers', max_numplayers: 10},
+        {name: 'Silicon Valley Slammers', max_numplayers: 8}
       ]);
     });
 }
